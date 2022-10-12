23018b801fe7219951b03c1f567b5c7ae106f0b8
aviv-raztkovski <12847141+aviv-raztkovski@users.noreply.github.com>
2022-10-12 18:50:38 +0300

    info section done

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -1,21 +1,20 @@
+import cardimg from '../assets/cardimg.png'
+
 export default function Info() {
     return (
         <div className="info">
-            <img src="" />
+            <img className="info--img" src={cardimg} />
             <h1 className="info--name">Aviv Raztkovski</h1>
             <h3 className="info--profession">FullStack Developer</h3>
-            <a className="info--webLink">Aviv.Raztkovski</a>
+            {/* <a className="info--webLink">Aviv.Raztkovski</a> */}
 
-            <div className="info--btns">
-                <button className="info--btns--btn btnWhite">
-                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-                        <title>envelop</title>
-                        <path d="M29 4h-26c-1.65 0-3 1.35-3 3v20c0 1.65 1.35 3 3 3h26c1.65 0 3-1.35 3-3v-20c0-1.65-1.35-3-3-3zM12.461 17.199l-8.461 6.59v-15.676l8.461 9.086zM5.512 8h20.976l-10.488 7.875-10.488-7.875zM12.79 17.553l3.21 3.447 3.21-3.447 6.58 8.447h-19.579l6.58-8.447zM19.539 17.199l8.461-9.086v15.676l-8.461-6.59z" />
-                    </svg>
-                        
-                    Email   
-                </button>
-            </div>
+            <button className="info--btn btnWhite">
+                <svg className="info--btn--icon" version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+                    <title>envelop</title>
+                    <path d="M29 4h-26c-1.65 0-3 1.35-3 3v20c0 1.65 1.35 3 3 3h26c1.65 0 3-1.35 3-3v-20c0-1.65-1.35-3-3-3zM12.461 17.199l-8.461 6.59v-15.676l8.461 9.086zM5.512 8h20.976l-10.488 7.875-10.488-7.875zM12.79 17.553l3.21 3.447 3.21-3.447 6.58 8.447h-19.579l6.58-8.447zM19.539 17.199l8.461-9.086v15.676l-8.461-6.59z" />
+                </svg>
+                <p className="info--btn--text">Email</p>   
+            </button>
         </div>
     )
 }
